98fc3e4641f1b09876c3bbb2c1d95fc06cc98e3d
Bryan-PNKN <Bryan-PNKN@github.com>
2025-01-08 10:26:07 +0100

    Deze werkt wel

--- present.py
+++ present.py
@@ -1,42 +1,42 @@
 import matplotlib.pyplot as plt
 
 # Needs to become a class
-data_dict: dict[tuple | tuple[str, int]] = {}
-data_dict[(5, 5)] = ("P", 0)
-data_dict[(5, 6)] = ("H", 1)
-data_dict[(4, 6)] = ("P", 2)
-data_dict[(4, 7)] = ("P", 3)
-data_dict[(5, 7)] = ("H", 4)
-data_dict[(6, 7)] = ("P", 5)
-data_dict[(6, 6)] = ("H", 6)
-data_dict[(6, 5)] = ("H", 7)
 
-# Needs to become another class
-def show_points(data: dict[tuple | tuple[str, int]]) -> None:
-    # Filter the points out of the data
-    # Polair
-    x_p = [item[0][0] for item in data.items() if item[1][0] == "P"]
-    y_p = [item[0][1] for item in data.items() if item[1][0] == "P"]
+class Protein:
+    
+    def __init__(self, sequence: str) -> None:
+        self.data: dict[tuple | tuple[str, int]] = {}
+        for i, char in enumerate(sequence):
+            self.data[(i, 0)] = (f"{char}", i)
 
-    # Hydrofobe
-    x_h = [item[0][0] for item in data.items() if item[1][0] == "H"]
-    y_h = [item[0][1] for item in data.items() if item[1][0] == "H"]
+    def show_points(self) -> None:
+        # Filter the points out of the data
+        # Polair
+        x_p = [item[0][0] for item in self.data.items() if item[1][0] == "P"]
+        y_p = [item[0][1] for item in self.data.items() if item[1][0] == "P"]
 
-    # Covalent bonds
-    x_l = [item[0][0] for item in data.items()]
-    y_l = [item[0][1] for item in data.items()]
+        # Hydrofobe
+        x_h = [item[0][0] for item in self.data.items() if item[1][0] == "H"]
+        y_h = [item[0][1] for item in self.data.items() if item[1][0] == "H"]
 
-    # Borders for plot
-    x_min, x_max, y_min, y_max = min(x_l), max(x_l), min(y_l), max(y_l)
+        # Covalent bonds
+        x_l = [item[0][0] for item in self.data.items()]
+        y_l = [item[0][1] for item in self.data.items()]
 
-    # Make the plot with dots and line
-    plt.plot(x_l, y_l, c = "black", alpha= 0.8, linewidth= 5)
-    plt.plot(x_p, y_p, "bo", markersize= 20)
-    plt.plot(x_h, y_h, "ro", markersize= 20)
-    plt.xlim(x_min - 1, x_max + 1)
-    plt.ylim(y_min - 1, y_max + 1)
-    plt.legend(["Bond", "Polair", "Hydrofobe"])
-    plt.show()
+        # Borders for plot
+        x_min, x_max, y_min, y_max = min(x_l), max(x_l), min(y_l), max(y_l)
+
+        # Make the plot with dots and line
+        plt.plot(x_l, y_l, c = "black", alpha= 0.8, linewidth= 5)
+        plt.plot(x_p, y_p, "bo", markersize= 20)
+        plt.plot(x_h, y_h, "ro", markersize= 20)
+        plt.xlim(x_min - 1, x_max + 1)
+        plt.ylim(y_min - 1, y_max + 1)
+        plt.legend(["Bond", "Polair", "Hydrofobe"])
+        plt.show()
+
+protein1 = Protein("PHPP")
+protein1.show_points()
 
 def options(y: int, x: int, data: dict[tuple | tuple[str, int]]) -> list[tuple[int]]:
     result = []
@@ -68,4 +68,4 @@ def calculate_stability(data: list[list[str | tuple[str, int]]]) -> int:
             if f"{option}" in filtered_dict and filtered_dict[f"{option}"] == "H":
                 print("ja")
 
-options(3, 5, data_dict)
+# show_points(een.data)
