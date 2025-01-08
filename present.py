999834d96f6665a7de23415326850b3a9024b44d
Bryan-PNKN <Bryan-PNKN@github.com>
2025-01-08 11:05:49 +0100

    Maybe interactive plotting implemented

--- present.py
+++ present.py
@@ -1,15 +1,13 @@
 import matplotlib.pyplot as plt
 
-# Needs to become a class
-
 class Protein:
     
     def __init__(self, sequence: str) -> None:
-        self.data: dict[tuple | tuple[str, int]] = {}
+        self.data: dict[tuple[int, int], tuple[str, int]] = {}
         for i, char in enumerate(sequence):
             self.data[(i, 0)] = (f"{char}", i)
 
-    def show_points(self) -> None:
+    def show_points(self) ->  None:
         # Filter the points out of the data
         # Polair
         x_p = [item[0][0] for item in self.data.items() if item[1][0] == "P"]
@@ -39,49 +37,38 @@ class Protein:
         if x_c:
             in_plot.append("Cystine")
 
-
-
         # Make the plot with dots and line
-        plt.plot(x_l, y_l, c = "black", alpha= 0.8, linewidth= 5)
-        plt.plot(x_p, y_p, "bo", markersize= 20)
-        plt.plot(x_h, y_h, "ro", markersize= 20)
-        plt.plot(x_c, y_c, "go", markersize= 20)
+        fig, ax = plt.subplots()
+        ax.plot(x_l, y_l, c = "black", alpha= 0.8, linewidth= 5)
+        ax.plot(x_p, y_p, "bo", markersize= 20)
+        ax.plot(x_h, y_h, "ro", markersize= 20)
+        ax.plot(x_c, y_c, "go", markersize= 20)
         plt.xlim(x_min - 1, x_max + 1)
         plt.ylim(y_min - 1, y_max + 1)
         plt.legend(in_plot)
         plt.show()
 
+    def options(self, x: int, y: int) -> None:
+        "Prints the the North, East, South and West coördinates"
+        x_diff = [0, 1, 0, -1]
+        y_diff = [1, 0, -1, 0]
+        result = [(x_diff[i] + x, y_diff[i] + y) for i in range(4)]
+        print(result)
+
 
 protein1 = Protein("HHPHPC")
+# protein1.options(5, 5)
 protein1.show_points()
 
 
-def options(y: int, x: int, data: dict[tuple | tuple[str, int]]) -> list[tuple[int]]:
-    result = []
-
-    # Define the bounds
-    # x-1, y
-    # x  , y+1 
-    # x+1, y
-    # x  , y-1
-    
-
-    for i in range(-1, 3):
-        print((i % 2) * i // abs(i), 1 - abs(i))
-        # if data[()]
-        # for j in range(2):
-    #         diff = abs(y - i ) + abs(x - j)
-    #         if diff == 1:
-    #             result.append((i, j))
-    return result
-
-def calculate_stability(data: list[list[str | tuple[str, int]]]) -> int:
-    filtered_list = list(filter(lambda x: x[0] == "H", filtered_list))
-    filtered_dict = {}
-    score = 0
-    for value in filtered_list:
-        filtered_dict[f"{value[2], value[3]}"] = value[0]
-    for value in filtered_list:
-        for option in options(value[2], value[3]):
-            if f"{option}" in filtered_dict and filtered_dict[f"{option}"] == "H":
-                print("ja")
+# def calculate_stability(data: list[list[str | tuple[str, int]]]) -> int:
+#     filtered_list = list(filter(lambda x: x[0] == "H", filtered_list))
+#     filtered_dict = {}
+#     score = 0
+#     for value in filtered_list:
+#         filtered_dict[f"{value[2], value[3]}"] = value[0]
+#     for value in filtered_list:
+#         for option in options(value[2], value[3]):
+#             if f"{option}" in filtered_dict and filtered_dict[f"{option}"] == "H":
+#                 print("ja")
+#     return 1
