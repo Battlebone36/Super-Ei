a9fa74996061a5f1e8ca2874560853a6af7ce72c
Bryan-PNKN <Bryan-PNKN@github.com>
2025-01-08 13:50:09 +0100

    new files

--- present.py
+++ present.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -31,48 +31,48 @@ class Protein:
     def give_data(self) -> dict[tuple[int, int], tuple[str, int]]:
         return self.data
 
-    def show_points(self) ->  None:
-        """Makes a plot of the protein"""
-        # Filter the points out of the data
-        # Polair
-        x_p = [item[0][0] for item in self.data.items() if item[1][0] == "P"]
-        y_p = [item[0][1] for item in self.data.items() if item[1][0] == "P"]
+    # def show_points(self) ->  None:
+    #     """Makes a plot of the protein"""
+    #     # Filter the points out of the data
+    #     # Polair
+    #     x_p = [item[0][0] for item in self.data.items() if item[1][0] == "P"]
+    #     y_p = [item[0][1] for item in self.data.items() if item[1][0] == "P"]
 
-        # Hydrofobe
-        x_h = [item[0][0] for item in self.data.items() if item[1][0] == "H"]
-        y_h = [item[0][1] for item in self.data.items() if item[1][0] == "H"]
+    #     # Hydrofobe
+    #     x_h = [item[0][0] for item in self.data.items() if item[1][0] == "H"]
+    #     y_h = [item[0][1] for item in self.data.items() if item[1][0] == "H"]
 
-        # Cystine        
-        x_c = [item[0][0] for item in self.data.items() if item[1][0] == "C"]
-        y_c = [item[0][1] for item in self.data.items() if item[1][0] == "C"]
+    #     # Cystine        
+    #     x_c = [item[0][0] for item in self.data.items() if item[1][0] == "C"]
+    #     y_c = [item[0][1] for item in self.data.items() if item[1][0] == "C"]
 
-        # Covalent bonds
-        x_l = [item[0][0] for item in self.data.items()]
-        y_l = [item[0][1] for item in self.data.items()]
+    #     # Covalent bonds
+    #     x_l = [item[0][0] for item in self.data.items()]
+    #     y_l = [item[0][1] for item in self.data.items()]
 
-        # Borders for plot and legend
-        borders = [min(x_l), max(x_l), min(y_l), max(y_l)]
+    #     # Borders for plot and legend
+    #     borders = [min(x_l), max(x_l), min(y_l), max(y_l)]
 
-        in_plot = []
-        if x_l:
-            in_plot.append("Bond")
-        if x_p:
-            in_plot.append("Polair")
-        if x_h:
-            in_plot.append("Hydrofobe")
-        if x_c:
-            in_plot.append("Cystine")
+    #     in_plot = []
+    #     if x_l:
+    #         in_plot.append("Bond")
+    #     if x_p:
+    #         in_plot.append("Polair")
+    #     if x_h:
+    #         in_plot.append("Hydrofobe")
+    #     if x_c:
+    #         in_plot.append("Cystine")
 
-        # Make the plot with dots and line
-        fig, ax = plt.subplots()
-        ax.plot(x_l, y_l, c = "black", alpha= 0.8, linewidth= 5)
-        ax.plot(x_p, y_p, "bo", markersize= 20)
-        ax.plot(x_h, y_h, "ro", markersize= 20)
-        ax.plot(x_c, y_c, "go", markersize= 20)
-        plt.xlim(min(borders) - 1, max(borders) + 1)
-        plt.ylim(min(borders) - 1, max(borders) + 1)
-        plt.legend(in_plot)
-        plt.show()
+    #     # Make the plot with dots and line
+    #     fig, ax = plt.subplots()
+    #     ax.plot(x_l, y_l, c = "black", alpha= 0.8, linewidth= 5)
+    #     ax.plot(x_p, y_p, "bo", markersize= 20)
+    #     ax.plot(x_h, y_h, "ro", markersize= 20)
+    #     ax.plot(x_c, y_c, "go", markersize= 20)
+    #     plt.xlim(min(borders) - 1, max(borders) + 1)
+    #     plt.ylim(min(borders) - 1, max(borders) + 1)
+    #     plt.legend(in_plot)
+    #     plt.show()
 
     def neighbours(self, coord: tuple[int, int]) -> list[tuple[int, int]]:
         "Prints the North, East, South and West coördinates of the one given"
@@ -171,8 +171,8 @@ class Protein:
 
 
 protein1 = Protein("HHPHPC")
-protein1.stability()
-protein1.show_points()
+# protein1.stability()
+# protein1.show_points()
 protein1.fold((2, 0), "left")
 protein1.fold((2, 2), "right")
 
---
+++ visualisation.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+from present import Protein
+
+class Visualise:
+    def show_points(self, data: dict[tuple[int, int], tuple[str, int]]) ->  None:
+        """Makes a plot of the protein"""
+        # Filter the points out of the data
+        # Polair
+        x_p = [item[0][0] for item in data.items() if item[1][0] == "P"]
+        y_p = [item[0][1] for item in data.items() if item[1][0] == "P"]
+
+        # Hydrofobe
+        x_h = [item[0][0] for item in data.items() if item[1][0] == "H"]
+        y_h = [item[0][1] for item in data.items() if item[1][0] == "H"]
+
+        # Cystine        
+        x_c = [item[0][0] for item in data.items() if item[1][0] == "C"]
+        y_c = [item[0][1] for item in data.items() if item[1][0] == "C"]
+
+        # Covalent bonds
+        x_l = [item[0][0] for item in data.items()]
+        y_l = [item[0][1] for item in data.items()]
+
+        # Borders for plot and legend
+        borders = [min(x_l), max(x_l), min(y_l), max(y_l)]
+
+        in_plot = []
+        if x_l:
+            in_plot.append("Bond")
+        if x_p:
+            in_plot.append("Polair")
+        if x_h:
+            in_plot.append("Hydrofobe")
+        if x_c:
+            in_plot.append("Cystine")
+
+        # Make the plot with dots and line
+        fig, ax = plt.subplots()
+        ax.plot(x_l, y_l, c = "black", alpha= 0.8, linewidth= 5)
+        ax.plot(x_p, y_p, "bo", markersize= 20)
+        ax.plot(x_h, y_h, "ro", markersize= 20)
+        ax.plot(x_c, y_c, "go", markersize= 20)
+        plt.xlim(min(borders) - 1, max(borders) + 1)
+        plt.ylim(min(borders) - 1, max(borders) + 1)
+        plt.legend(in_plot)
+        plt.show()
