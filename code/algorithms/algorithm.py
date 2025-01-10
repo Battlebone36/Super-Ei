44683f5222c63b5956685a9a97d46cda08f3712e
SydneyCelie <sydney@LAPTOP-R8B5G7IS>
2025-01-10 10:48:49 +0100

    start of algorithm

--- code/algorithms/algorithm.py
+++ code/algorithms/algorithm.py
@@ -1,8 +1,35 @@
 from code.classes.protein import Protein
 import random
 
-def random_fold(protein: Protein) -> Protein:
-    
+def random_fold(protein: Protein, attempts: int) -> Protein:
+    """Randomly folds a protein multiple times."""
+    sequence = ""
+
+    for i in range(len(protein.data)):
+        for coord, (amino, index) in protein.data.items():
+            if index == i:
+                sequence += amino
+                break
+
+    for attempt in range(attempts):
+        copy_protein = Protein(sequence)
+
+        for i in range(1, len(copy_protein.data) - 1):
+            current_coord = None
+            for coord, (amino, index) in copy_protein.data.items():
+                if index == i:
+                    current_coord = coord
+                    break
+
+            fold_choice = random.randint(0,2)
+
+            if fold_choice == 1:
+                copy_protein.is_foldable(current_coord)
+                copy_protein.fold(current_coord, "left")
+            elif fold_choice == 2:
+                copy_protein.is_foldable(current_coord)
+                copy_protein.fold(current_coord, "right")
+
     return protein
 
 protein1 = Protein("HHCPPPPH")
