80943218e51368b69f712f22078361ba51214b4d
Bryan-PNKN <Bryan-PNKN@github.com>
2025-01-10 12:15:34 +0100

    pycodestyle complete

--- code/classes/protein.py
+++ code/classes/protein.py
@@ -2,8 +2,7 @@ import numpy as np
 
 
 class Protein:
-    
-    def __init__(self, sequence: str, command: str = None) -> None:
+    def __init__(self, sequence: str, command: str | None = None) -> None:
         """
         Initialise the protein from a sequence of "P", "H" and "C" characters.
         The character with its order are stored in a dictionary where the keys are the coordinates.
@@ -11,10 +10,10 @@ class Protein:
         sequence = sequence.upper()
         self.data: dict[tuple[int, int], tuple[str, int]] = {}
         self.load_data(sequence, command)
-        self.left_turn = np.array([[0, -1], [1, 0]])
-        self.right_turn = np.array([[0, 1], [-1, 0]])
-    
-    def load_data(self, sequence: str, command = None) -> None:
+        self.left_turn = np.array([[0, -1], [1, 0]], dtype=int)
+        self.right_turn = np.array([[0, 1], [-1, 0]], dtype=int)
+
+    def load_data(self, sequence: str, command=None) -> None:
         """
         Load data into the protein library.
         If the command "manual" is not given the string is implemented in the data.
@@ -26,20 +25,20 @@ class Protein:
             return
         command.lower()
         if command == "manual" or command == "manuel":
-            self.data[(0, 0)]  = ("P", 0)
-            self.data[(1, 0)]  = ("C", 1)
-            self.data[(1, 1)]  = ("H", 2)
-            self.data[(0, 1)]  = ("H", 3)
-            self.data[(0, 2)]  = ("H", 4)
-            self.data[(1, 2)]  = ("P", 5)
-            self.data[(2, 2)]  = ("P", 6)
-            self.data[(2, 1)]  = ("H", 7)
-            self.data[(2, 0)]  = ("C", 8)
+            self.data[(0, 0)] = ("P", 0)
+            self.data[(1, 0)] = ("C", 1)
+            self.data[(1, 1)] = ("H", 2)
+            self.data[(0, 1)] = ("H", 3)
+            self.data[(0, 2)] = ("H", 4)
+            self.data[(1, 2)] = ("P", 5)
+            self.data[(2, 2)] = ("P", 6)
+            self.data[(2, 1)] = ("H", 7)
+            self.data[(2, 0)] = ("C", 8)
             self.data[(2, -1)] = ("H", 9)
             self.data[(1, -1)] = ("C", 10)
             self.data[(0, -1)] = ("P", 11)
             self.data = dict(sorted(self.data.items(), key=lambda item: item[1][1]))
-        
+
     def give_data(self) -> dict[tuple[int, int], tuple[str, int]]:
         return self.data
 
@@ -49,7 +48,7 @@ class Protein:
         y_diff = [1, 0, -1, 0]
         result = [(x_diff[i] + coord[0], y_diff[i] + coord[1]) for i in range(4)]
         return result
-    
+
     def type_bond(self, coord1: tuple[int, int], coord2: tuple[int, int]) -> int:
         """
         Gives back the stability of the bond that could be formed between the two amino acids.
@@ -66,10 +65,9 @@ class Protein:
             type_aminos.add(self.data[coord2][0])
             if type_aminos == {"H"} or type_aminos == {"C", "H"}:
                 return -1
-            elif type_aminos =={"C"}:
+            elif type_aminos == {"C"}:
                 return -5
         return 0
-
 
     def h_bond(self, coord1: tuple[int, int], coord2: tuple[int, int]) -> bool:
         """
@@ -91,7 +89,7 @@ class Protein:
             return ((self.data[coord1][0] == "C" and self.data[coord2][0] == "H") or
                     (self.data[coord1][0] == "H" and self.data[coord2][0] == "C"))
         return False
-    
+
     def c_bond(self, coord1: tuple[int, int], coord2: tuple[int, int]) -> bool:
         """
         Returns if the coordinates are in the protein dataset and
@@ -109,20 +107,18 @@ class Protein:
         for acid in self.data.items():
             if acid[1][0] == "H" or acid[1][0] == "C":
                 h_acids[acid[0]] = acid[1]
-                
+
         # Loop through the "H" acids and look to the neighbours
         score = 0
         for acid in h_acids.items():
             friends = self.neighbours(acid[0])
             for friend in friends:
-                
                 if friend in self.data and abs(acid[1][1] - self.data[friend][1]) == 1:
                     continue
                 else:
                     score += self.type_bond(acid[0], friend)
         score //= 2
         return score
-
 
     def rotate_coord(self, coord: tuple[int, int], pivot: tuple[int, int],  matrix) -> tuple[int, int]:
         """
@@ -131,26 +127,27 @@ class Protein:
         Input:
         - coord: the point that is being rotated.
         - pivot: is the point around which the coord is rotated.
-        - matrix: the rotation matrix that defines the direction of the rotation.
+        - matrix: the matrix that defines the direction of the rotation.
 
         Output:
-        - Rotated coordinate        
+        - Rotated coordinate
         """
         rel_coord = (coord[0] - pivot[0], coord[1] - pivot[1])
-        v_rel_coord = np.array(rel_coord)
+        v_rel_coord = np.array(rel_coord, dtype=int)
         rel_rot_coord = matrix @ v_rel_coord
         rel_new_coord = tuple(rel_rot_coord)
         new_coord = (rel_new_coord[0] + pivot[0], rel_new_coord[1] + pivot[1])
         return new_coord
-    
+
     def is_foldable(self, pivot: tuple[int, int], matrix) -> tuple[bool, dict[tuple[int, int], tuple[str, int]]] | bool:
         # Store the points that are following in the sequence
         # These are following in the sequence
         points_to_rot: dict[tuple[int, int], tuple[str, int]] = {}
-        for acid in self.data.items():
-            if acid[1][1] > self.data[pivot][1]:
-                points_to_rot[acid[0]] = acid[1]
         
+        for coord, amino in self.data.items():
+            if amino[1] > self.data[pivot][1]:
+                points_to_rot[coord] = amino
+
         # Check if the rotation doesn't clash with the existing folding
         for acid in points_to_rot:
             new_coord = self.rotate_coord(acid, pivot, matrix)
@@ -163,8 +160,8 @@ class Protein:
         # Raise an error if the coordinate is not in the dataset
         if pivot not in self.data:
             print("coordinate not in dataset")
-            raise(IndexError)
-        
+            raise IndexError
+
         # Make the command case insensitive and define the rotation matrix
         direction = direction.lower()
         if direction == "right":
@@ -174,7 +171,9 @@ class Protein:
 
         # Rotate every point
         if (info := self.is_foldable(pivot, rot_matrix)):
-            points_to_rot = info[1]
+            valid, points_to_rot = info
+
+            # points_to_rot = dictionary
             for acid in points_to_rot:
                 new_coord = self.rotate_coord(acid, pivot, rot_matrix)
                 store_value = self.data.pop(acid)
@@ -189,17 +188,16 @@ class Protein:
         """
         # If the x coordinate is the same look at the y coordinate
         if coord2[0] == coord1[0]:
-                if coord2[1] - coord1[1] == 1:
-                    return 2
-                else:
-                    return -2
+            if coord2[1] - coord1[1] == 1:
+                return 2
+            else:
+                return -2
         # Else the y coordinates are the same thus look at the x coordinate
         else:
             if coord2[0] - coord1[0] == 1:
                 return 1
             else:
-                return -1 
-
+                return -1
 
     def output(self) -> str:
         """
@@ -212,16 +210,13 @@ class Protein:
             if amino == (0, 0):
                 fold_commands += f"{self.data[amino][0]},"
                 continue
-            
-            direction = self.check_direction(amino, previous_amino)
-            
-            fold_commands += f"{direction}\n{self.data[amino][0]},"
 
+            direction = self.check_direction(amino, previous_amino)
+            fold_commands += f"{direction}\n{self.data[amino][0]},"
             previous_amino = amino
 
         fold_commands += f"0\nscore,{self.stability()}"
         return fold_commands
-            
+
 
 protein1 = Protein("CHPHHPHC")
-
