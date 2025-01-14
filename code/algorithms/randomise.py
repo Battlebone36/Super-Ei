776e55080ea539f716df90c28377d64fb2dfc0e5
SydneyCelie <sydney@LAPTOP-R8B5G7IS>
2025-01-14 10:53:32 +0100

    cleanup random algorithm code

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -8,13 +8,6 @@ def random_fold(protein: Protein) -> Protein:
     """Randomly folds a protein multiple times and returns the folded protein."""
     sequence = protein.sequence
 
-    # # Stores the protein sequence
-    # for i in range(len(protein.data)):
-    #     for coord, (amino, index) in protein.data.items():
-    #         if index == i:
-    #             sequence += amino
-    #             break
-
     # Keep folding the protein randomly
     directions = ["x_pos", "x_neg", "y_pos", "y_neg", "z_pos", "z_neg"]
 
@@ -35,15 +28,4 @@ def random_fold(protein: Protein) -> Protein:
             if copy_protein.is_foldable(current_coord, copy_protein.rotations[fold_direction]):
                 copy_protein.fold(current_coord, fold_direction)
             
-            # if copy_protein.is_foldable(current_coord, getattr(copy_protein, fold_direction)):
-            #     copy_protein.fold(current_coord, fold_direction)
-            # elif fold_choice == 1 and copy_protein.is_foldable(current_coord, copy_protein.right_turn):
-            #     copy_protein.fold(current_coord, "right")
-
     return copy_protein
-
-
-# protein1 = Protein("HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH")
-# folded_protein = random_fold(protein1, 60)
-# visualise_protein(folded_protein)
-# print(folded_protein.give_data())
