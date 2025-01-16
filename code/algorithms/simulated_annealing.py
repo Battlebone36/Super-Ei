1b2d556c9ea0d475b25ff14e70b4a67480ba8f2f
Bryan-PNKN <Bryan-PNKN@github.com>
2025-01-16 14:25:44 +0100

    add foto's

--- code/algorithms/simulated_annealing.py
+++ code/algorithms/simulated_annealing.py
@@ -11,7 +11,7 @@ def simulated_annealing(protein: Protein) -> Protein:
     Sometimes worse folds are accepted, depending on the current temperature.
     """
     # Starting values
-    initial_temp: int = 50
+    initial_temp: int = 100
     cooling_rate: int = 0.99
     min_temp = 1
 
