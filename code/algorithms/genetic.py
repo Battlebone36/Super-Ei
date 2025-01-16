295d185b3802489612f6dae1038b1166c9608c31
SydneyCelie <sydney@LAPTOP-R8B5G7IS>
2025-01-16 16:05:03 +0100

    presentation details

--- code/algorithms/genetic.py
+++ code/algorithms/genetic.py
@@ -2,26 +2,25 @@ from code.classes.protein import Protein
 import copy
 from code.visualisation.visualisation import *
 import random
-random.seed(0)
+# random.seed(0)
 # import numpy as np
 
 def genetic(protein: Protein) -> None:
     proteins_dict: dict[int, tuple[Protein, list[int]]] = {}
+
+    # Generation 0
     for i in range(2):
         folds = [random.randint(0, 6) for i in range(len(protein.sequence) - 2)]
         proteins_dict[i] = (Protein(protein.sequence), folds)
         for j, DNA in enumerate(folds):
-            ans = proteins_dict[i][0].fold_by_DNA(DNA=DNA, index=j + 1)
-            possible_folds = {0, 1, 2, 3, 4, 5, 6}
-            while (not proteins_dict[i][0].fold_by_DNA(DNA=DNA, index=j + 1) and possible_folds):
-            # if ans is False:
-                         
-                impossible_folds = set()
-                impossible_folds.add(DNA)
-                possible_folds = possible_folds - impossible_folds
-                DNA = random.choice(list(possible_folds))
-
-                print("oh nee", impossible_folds, DNA)
+            proteins_dict[i][0].fold_by_DNA(DNA=DNA, index=j + 1)
+            
+            # Add stability to the already existing dictionary 
+            # Split the two best proteins in two -> and add these together -> based on stability (include weighted options)
+                # half 1.1 + half 2.2
+                # half 2.1 + half 1.2
+            # Keep it going until it can't be done anymore
+            
 
 
 
---
+++ static/presentation.md
@@ -0,0 +1,27 @@
+- Introductie
+    * het probleem - case uitleg
+        - doel -> functie diie gebruikt wordt(?)
+        - bindingen die mogelijk zijn en de stabiliteit score die horen bij de bindingen
+    * constraints
+        - hard constraints
+        - soft constraints
+    * state space
+        - gebruikte functie
+        - waar houdt het rekening mee -> assumpties
+- Theorie
+- Methoden
+    * eerst alle algoritmen doorlopen
+        - constructief
+        - iteratief
+    * pseudo code van de algoritmen
+    * docs samenhang classes en dergelijke -> docs van Radio Russia
+- Resultaten
+    * alle resultaten van de verschillende algoritmen
+    * plot
+    * Waarom zou het kunnen afwijken?
+- Conclusie
+    * Beste resultaat tonen met het algoritme dat het heeft gevonden
+- Discussie
+
+
+GEEN CODE GEBRUIKEN IN DE PRESENTATIE
