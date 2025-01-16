7cd2469d4a9ce9d06e8f7a7f928dcab876d296ba
SydneyCelie <sydney@LAPTOP-R8B5G7IS>
2025-01-16 10:57:38 +0100

    docstring function main

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import csv
 
 def write_output(protein: Protein):
     """
+    Writes the output of a Protein object to a CSV file in a specific format.
     """
     with open ('output.csv','w',newline = '') as csvfile:
         my_writer = csv.writer(csvfile, delimiter = ',')
