1ff59d91e9e27df8c0dc3c07068ca588803e1c5e
Bryan-PNKN <Bryan-PNKN@github.com>
2025-01-17 14:38:58 +0100

    main cleaning

--- main.py
+++ main.py
@@ -17,27 +17,5 @@ def write_output(protein: Protein):
 
 
 if __name__ == "__main__":
-    # protein_vis = Protein("HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH")
-    
-    # visualise_protein(simulated_annealing(protein_vis))
-
-    # greedy_search_sequence(protein_vis)
-    # protein_vis.add_amino((0, 0, 0), "H", 0)
-    # new_prot = random_fold_grow.algorithm(protein_vis)
-    # print(protein_vis())
-    # visualise_protein(random_fold_grow.algorithm(protein_vis))
-    # print(protein_vis.fold_by_DNA(5, 4))
-    # # visualise_protein(even_better_climbing_fold(protein_vis))
-    # visualise_protein(protein_vis)
-
-    # algorithms = [greedy_search_sequence, climbing_fold, better_climbing_fold, even_better_climbing_fold]
-    # algorithms = [random_fold, greedy_search_sequence, climbing_fold]
-    # visualise_algorithm(algorithms, "together")
-    # algorithms = [greedy_search_sequence, climbing_fold, better_climbing_fold, even_better_climbing_fold]
-    # algorithms = [random_fold, random_fold_grow.random_fold2]
     algorithms = [random_fold, greedy_fold, greedy_search_sequence]
-    # visualise_algorithm(algorithms)
-    visualise_algorithm_data(algorithms, "time-stability")
-
-    # protein1 = Protein("HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH")
-    
+    visualise_algorithm_data(algorithms, "time-stability")    
