54581e824321ff8469462c7aa48c138626ca951e
Battlebone36 <ronan@Ronan>
2025-01-27 11:46:46 +0100

    small changes

--- script1.py
+++ script1.py
@@ -6,16 +6,6 @@ if __name__ == "__main__":
                 SimulatedAnnealing,
                 Gen_2
                   ]
-    
-    # algorithms_vis = [  Genetic,
-    #                     Gen_1,
-    #                     Gen_2,
-    #                     Gen_3,
-    #                     Gen_4,
-    #                     Gen_5,
-    #                     Gen_6,
-    #                     Gen_7
-    #                     ]
     # for i in range(100):
     #     print(i)
     #     for algorithm in algorithms:
